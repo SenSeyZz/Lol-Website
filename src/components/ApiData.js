19fc97750cbdcb4e39b5cebb7a222241f97efc46
Paul Frappier <paul.frappier@hotmail.fr>
2023-03-29 15:54:47 +0200

    being able to access multiple matches

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
+import parse from 'html-react-parser';
 
 // My own imports
 import * as Name from "./API/GetName"
@@ -9,131 +10,105 @@ import * as MatchStats from "./API/GetStats"
 
 export function ApiData(){
 
+ const [stock, setStock] = useState([])
+ const [participant, setParticipant] = useState([MatchStats.participants])
  const [summonerName, setSummonerName] = useState([])
- const [matchStatsChamp, setMatchStatsChamp] = useState([])
- const [matchStatsWin, setMatchStatsWin] = useState([])
- const [matchStatsDeath, setMatchStatsDeath] = useState([])
- const [matchPosition, setMatchPosition] = useState([])
- const [matchChallengeDamagePerMinute, setMatchChallengeDamagePerMinute] = useState([])
- const [matchCs10, setMatchCs10] = useState([])
- const [matchCsAdvantage, setMatchCsAdvantage] = useState([])
- const [matchLvlAdvantage, setMatchLvlAdvantage] = useState([])
- const [matchDodges, setMatchDodges] = useState([])
- const [matchTeamDamage, setMatchTeamDamage] = useState([])
+ 
+
+ useEffect(() => {
+  setParticipant(MatchStats.participants)
+}, [MatchStats.participants])
 
  async function gameStats(){
 
+    
+    let i = 0
+    
+    var csBefore10
+    var wOrL
+    let stock = []
     var statistiques
-    var participant = MatchStats.participants
+  
+         
+      console.log(participant);
+    
 
     //Loop through the participants to get the one with matching PUUID and then get the stats we want
-    for (let i = 0; i < participant.length; i++) {
-      if (participant[i].puuid === Puuid.puuid) {
-         statistiques= {
-          //Normal stats
-          name: participant[i].championName,
-          win: participant[i].win,
-          death : participant[i].deaths,
-          position: participant[i].individualPosition,
+    for ( i = 0; i < participant.length; i++) {
+      let participantGame = participant[i]
+      
 
-          // Stats from the challenges part
-          damagePerMinute: Math.round(participant[i].challenges.damagePerMinute),
-          damagePercent: Math.round(participant[i].challenges.teamDamagePercentage *100),
-          totalDamage: participant[i].totalDamageDealt,
-
-          csBefore10: participant[i].challenges.laneMinionsFirst10Minutes,
-          csAdvantage: participant[i].challenges.maxCsAdvantageOnLaneOpponent,
-
-          lvlAdvantage: participant[i].challenges.maxLevelLeadLaneOpponent,
-          dodges: participant[i].challenges.skillshotsDodged,
-        
-      };
-      //Set all the values to the ones we got from the API
-        // Idea: jsut make One constant and set it the value of all the variables (i.e setResult({summonerName}, He was playing {{statistiques.name} ... ))
-        setMatchStatsChamp(
-        <>
-          was playing <span style={{ fontWeight: 'bold' }}>{statistiques.name}</span>.
-        </>)
-        
-        setMatchStatsDeath(
-          <>
-            He died <span style={{ fontWeight: 'bold' }}>{statistiques.death}</span> times during this game
-          </>
-          
-          )
-        setMatchPosition(
-          <>
-            His role was <span style={{ fontWeight: 'bold' }}>{statistiques.position.toLowerCase()}.</span>
-          </>
-          
-          )
-
-        setMatchChallengeDamagePerMinute(
-          <>
-            He dealt <span style={{ fontWeight: 'bold' }}>{statistiques.damagePerMinute}</span> damage per minute or <span style={{ fontWeight: 'bold' }}> {statistiques.totalDamage}</span> total.
-          </>
-          
-          )
-        setMatchTeamDamage(
-          <>
-            Which represents <span style={{ fontWeight: 'bold' }}>{statistiques.damagePercent}%</span> of his team total damage
-          </>
-          
-          )
+      for (let j = 0; j< participantGame.length  ; j++){
        
+        if (participantGame[j].puuid === Puuid.puuid) {
+          statistiques= {
+           //Normal stats
+           name: participantGame[j].championName,
+           win: participantGame[j].win,
+           death : participantGame[j].deaths,
+           position: participantGame[j].individualPosition,
+ 
+           // Stats from the challenges part
+           damagePerMinute: Math.round(participantGame[j].challenges.damagePerMinute),
+           damagePercent: Math.round(participantGame[j].challenges.teamDamagePercentage *100),
+           totalDamage: participantGame[j].totalDamageDealt,
+ 
+           csBefore10: participantGame[j].challenges.laneMinionsFirst10Minutes,
+           csAdvantage: Math.round(participantGame[j].challenges.maxCsAdvantageOnLaneOpponent),
+ 
+           lvlAdvantage: participantGame[j].challenges.maxLevelLeadLaneOpponent,
+           dodges: participantGame[j].challenges.skillshotsDodged,
+         
+       };
+       //Set all the values to the ones we got from the API
+         // Idea: jsut make One constant and set it the value of all the variables (i.e setResult({summonerName}, He was playing {{statistiques.name} ... ))
 
-        setMatchCsAdvantage(
-          <>
-          He had <span style={{ fontWeight: 'bold' }}>{statistiques.csAdvantage}</span> CS more than his lane opponent
-          </>
-          
-          )
-        setMatchLvlAdvantage(
-          <>
-            And was <span style={{ fontWeight: 'bold' }}>{statistiques.lvlAdvantage}</span> levels ahead.
-          </>
-          
-          )
-        setMatchDodges(
-        <>
-          He dodged <span style={{ fontWeight: 'bold' }}>{statistiques.dodges}</span> skill shots.
-        </>
-        
-        )       
+         if(statistiques.csBefore10 <= 50){
+           
+          csBefore10 = " Not even 50 he sucks at farming."
+                      
+         }else if (50 <statistiques.csBefore10 < 100){
+           
+          csBefore10 = " Good job."
+             
+         }else {
+           
+          csBefore10 =" perfect CS ! Respect."
+            
+         }
 
-        if(statistiques.csBefore10 <= 50){
-          setMatchCs10(
-            <>
-              He had <span style={{fontWeight: "bold"}}>{statistiques.csBefore10}</span> CS before 10 minutes. Not even 50 he sucks at farming.
-            </>           
-            )
-        }else if (50 <statistiques.csBefore10 < 100){
-          setMatchCs10(
-            <>
-              He had <span style={{fontWeight: "bold"}}>{statistiques.csBefore10}</span> CS before 10 minutes Good job.
-            </>          
-            )
 
-        }else {
-          setMatchCs10(
-            <>
-              He had <span style={{fontWeight: "bold"}}>{statistiques.csBefore10}</span> CS before 10 minutes perfect CS ! Respect.
-            </>
-            )
+         var winOrLoose = statistiques.win
+
+         if(winOrLoose){
+          wOrL = " He won this match"
+        }else{
+          wOrL = "He lost this match"
+         
         }
 
-        var winOrLoose = statistiques.win
 
-      
-      if(winOrLoose){
-        setMatchStatsWin(" He won this match")
-      }else{
-        setMatchStatsWin("He lost this match")
+        
+          var sentence = `
+              Match number ${i+1}, He was playing <strong>${statistiques.name}</strong>. His role was <strong>${statistiques.position.toLowerCase()}.</strong> ${wOrL}<br/>
+              He died <strong>${statistiques.death}</strong> times during this game<br/>
+              He dealt <strong>${statistiques.damagePerMinute}</strong> damage per minute or <strong> ${statistiques.totalDamage}</strong> total.<br/>
+              Which represents <strong>${statistiques.damagePercent}%</strong> of his team total damage.<br/>
+              He had <strong>${statistiques.csBefore10}</strong> CS before 10 minutes.${csBefore10} He had <strong>${statistiques.csAdvantage}</strong> CS more than his lane opponent. And was <strong>${statistiques.lvlAdvantage}</strong> levels ahead.<br/>
+              He dodged <strong>${statistiques.dodges}</strong> skill shots.<br/><br/>`
 
-      }
-    }  
-  }
+
+          stock.push(parse(sentence) )
+    
+      }  
+    
+     setStock(stock)
+
+   }
+ }
+ 
 }
+
 
 
 
@@ -143,9 +118,11 @@ async function usePromise(){
     await Name.name()
     setSummonerName(Name.charName)
     await Puuid.getPuuid()
-    await MatchId.getMatchId()
-    await MatchStats.getMatchstats()
+    await MatchId.getMatchId()  
+    await MatchStats.getMatchstats() 
     await gameStats()
+    
+    
 }
 
 
@@ -157,12 +134,11 @@ async function usePromise(){
           <textarea className='form-control' id='SummName' placeholder="Enter Your Summoner's Name"></textarea>
           <button className='btn btn-primary ml-2' onClick={usePromise}>Submit</button>
         </div>
+        <input style={{width: 300}} type="number" placeholder='How many games do you want ?' id="gameNb"></input>
         
-          <p className='text-md-left'>{summonerName} {matchStatsChamp} {matchPosition} {matchStatsWin}</p>
-          <p className='text-md-left'>{matchStatsDeath}</p>
-          <p className='text-md-left'>{matchChallengeDamagePerMinute} {matchTeamDamage} </p>
-          <p className='text-md-left'>{matchCs10} {matchCsAdvantage} {matchLvlAdvantage}</p>
-          <p className='text-md-left'>{matchDodges}</p>      
+        <p className='text-md-left'>{summonerName}</p>
+        <p>{stock}</p>
+             
       </div>
     </div>
       )
